3f18fc142b5c5adeae34edaaa0ca1145cbf76be4
jaferIdrees <jaferll@gmail.com>
2022-09-28 23:53:45 +0300

    Add State class

--- app/main.rb
+++ app/main.rb
@@ -15,50 +15,57 @@ class State
     @teachers = []
   end
 
+  def choices
+    choices = {
+      1 => "List all books",
+      2 => "List all people",
+      3 => "Create a person",
+      4 => "Create a book",
+      5 => "Create a rental",
+      6 => "List all rentals for a person id",
+      7 => "Exit",
+    }
+  end
 
-end
-def choices
-  choices = {
-    1 => 'List all books',
-    2 => 'List all people',
-    3 => 'Create a person',
-    4 => 'Create a book',
-    5 => 'Create a rental',
-    6 => 'List all rentals for a person id',
-    7 => 'Exit'
-  }
-end
+  # Method to handle user choice
+  def handle_choice(choice)
+    case choice
+    when 1
+    when 2
+    when 3
+      print "Do you want to create a student (1) ot a teacher (2)?[Input a number]:"
+      person_choice = gets.chomp
+      state.create_person(person_choice)
+    when 4
+    when 5
+    when 6
+    when 7
+    else
+      puts "Invalid choice"
+      main
+    end
+  end
 
-# Method to handle user choice
-def handle_choice(choice)
-  case choice
-  when 1
-  when 2
-  when 3
-    print 'Do you want to create a student (1) ot a teacher (2)?[Input a number]:'
-    person_choice = gets.chomp
-    state.create_person(person_choice)
-  when 4
-  when 5
-  when 6
-  when 7
-  else
-    puts 'Invalid choice'
-    main
+  def main_menu
+    puts "Welcome to School library App!"
+    puts ""
+    puts "Please choose an option by entering a number:"
+    choices.each { |key, value| puts "#{key}:#{value}" }
+    print "Please enter a number:"
+    choice = gets.chomp
+    handle_choice(choice.to_i)
+    puts choices[choice.to_i]
+  end
+
+  def run
+    main_menu
   end
 end
+
 # Main method
 def main
-
-  state if state.nil?
-  puts 'Welcome to School library App!'
-  puts ''
-  puts 'Please choose an option by entering a number:'
-  choices.each { |key, value| puts "#{key}:#{value}"}
-  print 'Please enter a number:'
-  choice = gets.chomp
-  handle_choice(choice.to_i)
-  puts choices[choice.to_i]
+  app = State.new
+  app.run
 end
 
 main
