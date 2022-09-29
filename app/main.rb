e59891ee5460dd79fba4354f1d954facaf521387
jaferIdrees <jaferll@gmail.com>
2022-09-29 18:01:05 +0300

    Add create student and teacher metrhods

--- app/main.rb
+++ app/main.rb
@@ -1,57 +1,79 @@
-# frozen_string_literal: true
+require_relative 'classes/student'
+require_relative 'classes/teacher'
 
-# $choices = %w(
-#   List\ all\ books
-#   List\ all\ people
-#   Create\ a\ person
-#   Create\ a\ book
-#   Create\ a\ rental
-#   List\ all\ rentals\ for\ a\ person\ id
-#   Exit)
+# State class to store app's state
 class State
+
   def initialize
     @books = []
-    @students = []
-    @teachers = []
+    @people = []
   end
 
   def choices
-    choices = {
-      1 => "List all books",
-      2 => "List all people",
-      3 => "Create a person",
-      4 => "Create a book",
-      5 => "Create a rental",
-      6 => "List all rentals for a person id",
-      7 => "Exit",
+    @choices = {
+      1 => 'List all books',
+      2 => 'List all people',
+      3 => 'Create a person',
+      4 => 'Create a book',
+      5 => 'Create a rental',
+      6 => 'List all rentals for a person id',
+      7 => 'Exit'
     }
+  end
+
+  def create_student
+    print 'Age:'
+    age = gets.chomp
+    print 'Name:'
+    name = gets.chomp
+    print 'Has parent permission?[Y/N]:'
+    permission = gets.chomp
+    @people << Student.new(_, name, age, permission == Y)
+  end
+  
+  def create_teacher
+    print 'Age:'
+    age = gets.chomp
+    print 'Name:'
+    name = gets.chomp
+    print 'Specialization:'
+    specialization = gets.chomp
+    @people << Teacher.new(specialization, name, age)
+  end
+
+  def create_book
+    print 'Title:'
+    title = gets.chomp
+    
   end
 
   # Method to handle user choice
   def handle_choice(choice)
     case choice
     when 1
-    when 2
-    when 3
-      print "Do you want to create a student (1) ot a teacher (2)?[Input a number]:"
+    when 'List all people'
+      @people.each { |perosn| }
+    when 'Create a person'
+      print 'Do you want to create a student (1) ot a teacher (2)?[Input a number]:'
       person_choice = gets.chomp
-      state.create_person(person_choice)
-    when 4
+      person_choice == 1 ? state.create_student : state.create_teacher
+    when 'Create a book'
+      create_book
     when 5
     when 6
     when 7
     else
-      puts "Invalid choice"
-      main
+      puts 'Invalid choice'
+      main_menu
     end
   end
 
   def main_menu
-    puts "Welcome to School library App!"
-    puts ""
-    puts "Please choose an option by entering a number:"
+    puts 'Welcome to School library App!'
+    puts ''
+    puts 'Please choose an option by entering a number:'
     choices.each { |key, value| puts "#{key}:#{value}" }
-    print "Please enter a number:"
+    print 'Please enter a number:'
     choice = gets.chomp
     handle_choice(choice.to_i)
     puts choices[choice.to_i]
